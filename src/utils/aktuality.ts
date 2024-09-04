8db3c6fd79e0dd7ac1d7d0eca8274817938b264e
jackie4u <jackie4u@seznam.cz>
2024-09-04 23:22:57 +0200

    Updated images

--- src/utils/aktuality.ts
+++ src/utils/aktuality.ts
@@ -2,32 +2,32 @@ const aktuality = [
     {
       title: "Užijte si podzimní cyklovýlety naplno!",
       description: "Podzimní měsíce jsou ideální pro cyklistiku. Babí léto nabízí krásné počasí a ubývající provoz na cyklostezkách. Připravte si kolo na sezonu a vyrazte objevovat krásy okolí!",
-      image: "https://cdn.pixabay.com/photo/2023/11/06/04/53/woman-8368830_1280.jpg"
+      image: "/images/aktuality/podzim.jpg"
     },
     {
       title: "Nové dětské sedačky na skladě!",
       description: "Připravte se na cyklistické výlety s celou rodinou. Máme nové dětské sedačky na kola skladem! Přijďte si vybrat tu nejlepší pro vašeho malého cyklistu.",
-      image: "https://media.istockphoto.com/id/841442202/cs/fotografie/otec-na-kole-se-sv%C3%BDm-synem-na-d%C4%9Btsk%C3%A9-seda%C4%8Dce.jpg?s=2048x2048&w=is&k=20&c=E9yfyDdlwUjTS3N4s_mIP4UlSjX3Wu8Fn_gFj3J2WAo="
+      image: "/images/aktuality/detska-sedacka.jpg"
     },
     {
-      title: "Servisní prohlídka zdarma!",
-      description: "Připravte své kolo na zimní období. Nabízíme bezplatnou servisní prohlídku, kde zkontrolujeme stav vašeho kola a doporučíme případný servis.",
-      image: "https://media.istockphoto.com/id/615281154/cs/fotografie/mechanik-opravuj%C3%ADc%C3%AD-zadn%C3%AD-kolo-j%C3%ADzdn%C3%ADho-kola.jpg?s=1024x1024&w=is&k=20&c=-Hv_SagLT5AWqsnRdIO3B3sMzk-V6nU4mX_khVJzFjk="
+      title: "Nečekejte, až opadá listí z dubu",
+      description: "Léto sice ještě nekončí, ale podzim se blíží. Připravte své kolo na chladnější počasí a nechte si ho u nás zkontrolovat. Získáte i nezávaznou nabídku na případné opravy. Nečekejte, až bude pozdě a objednejte se na servis ještě dnes!",
+      image: "/images/aktuality/servis.jpg"
     },
     {
       title: "Nové elektrokola na skladě!",
       description: "Hledáte ekologický a pohodlný způsob dopravy? Přijďte si vyzkoušet naše nové elektrokola. Nabízíme široký výběr modelů pro všechny typy jezdců.",
-      image: "https://media.istockphoto.com/id/1506986834/cs/fotografie/p%C5%AFj%C4%8Dovna-elektrokol-zaparkovan%C3%A1-v-bl%C3%ADzkosti-obchodn%C3%ADho-centra-v-centru-polska-e-bike.jpg?s=1024x1024&w=is&k=20&c=zwX1o81ZeiH4ocfwnsiErGL3boSBj83XOav6_ySIuKg="
+      image: "/images/aktuality/elektro-kolo.jpg"
     },
     {
       title: "Slevy na příslušenství!",
       description: "Využijte naší aktuální nabídky slev na cyklistické příslušenství. Najdete u nás helmy, zámky, blatníky, pumpy a další potřebné vybavení.",
-      image: "https://cdn.pixabay.com/photo/2014/02/28/13/06/bear-276875_1280.jpg"
+      image: "/images/aktuality/medvidek.jpg"
     },
     {
       title: "Bezpečnost na prvním místě!",
       description: "Jezdíte rádi za tmy? Nezapomeňte na bezpečnostní vybavení! Nabízíme široký výběr cyklistických přileb, reflexních vest a světel. Vaše bezpečnost je naší prioritou!",
-      image: "https://cdn.pixabay.com/photo/2020/05/11/12/08/bicycle-helmet-5157835_960_720.jpg"
+      image: "/images/aktuality/helma.jpg"
     },
   ];  
 
--- src/utils/znacky.ts
+++ src/utils/znacky.ts
@@ -1,8 +1,8 @@
 const znacky = [
   { name: "Author", website: "http://cz.author.eu/", logo: "https://cz.author.eu/system/img/author_logo.png" },
-  { name: "Bianchi", website: "http://www.bianchicz.com/", logo: "https://www.bianchi.com/wp-content/themes/bianchi/inc/assets/images/logo-bianchi-white.svg" },
-  { name: "4ever", website: "http://www.4ever.cz/", logo: "https://www.4ever.cz/images/logo.svg" }, 
-  { name: "Scott", website: "https://www.scott-sports.com/", logo: "https://www.scott-sports.com/_ui/responsive/theme-scott/images/mainnav-logo.svg" },
+  // { name: "Bianchi", website: "http://www.bianchicz.com/", logo: "https://www.bianchi.com/wp-content/themes/bianchi/inc/assets/images/logo-bianchi-white.svg" },
+  // { name: "4ever", website: "http://www.4ever.cz/", logo: "https://www.4ever.cz/images/logo.svg" }, 
+  { name: "Scott", website: "https://www.scott-sports.com/cz/cs/", logo: "https://www.scott-sports.com/_ui/responsive/theme-scott/images/mainnav-logo.svg" },
   { name: "Superior", website: "http://superiorbikes.eu/cz", logo: "/images/znacky/superior.jpg" }, 
   { name: "Dema", website: "https://www.dema.bike/", logo: "/images/znacky/dema.png" },
 ];
