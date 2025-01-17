7d0f63c1adbc886edc26921f65f44afd967c9282
jackie4u <jackie4u@seznam.cz>
2025-01-17 14:58:07 +0100

    Aktualizovaná otevírací doba během jarních prázdnin

--- src/utils/aktuality.ts
+++ src/utils/aktuality.ts
@@ -1,34 +1,34 @@
 const aktuality = [
-    {
-      title: "Nečekejte, až opadá listí z dubu!",
-      description: "Podzim už je v plném proudu. Připravte své kolo na chladnější počasí a nechte si ho u nás zkontrolovat. Získáte i nezávaznou nabídku na případné opravy. Nečekejte, až bude pozdě a objednejte se na servis ještě dnes!",
-      image: "/images/aktuality/servis.jpg"
-    },
-    {
-      title: "Užijte si podzimní cyklovýlety naplno!",
-      description: "Podzimní měsíce jsou ideální pro cyklistiku. Babí léto nabízí krásné počasí a ubývající provoz na cyklostezkách. Připravte si kolo na sezonu a vyrazte objevovat krásy okolí!",
-      image: "/images/aktuality/podzim.jpg"
-    },
-    {
-      title: "Nové dětské sedačky na skladě!",
-      description: "Připravte se na cyklistické výlety s celou rodinou. Máme nové dětské sedačky na kola skladem! Přijďte si vybrat tu nejlepší pro vašeho malého cyklistu.",
-      image: "/images/aktuality/detska-sedacka.jpg"
-    },
-    {
-      title: "Nová elektrokola na skladě!",
-      description: "Hledáte ekologický a pohodlný způsob dopravy? Přijďte si vyzkoušet naše nová elektrokola. Nabízíme široký výběr modelů pro všechny typy jezdců.",
-      image: "/images/aktuality/elektro-kolo.jpg"
-    },
-    {
-      title: "Slevy na příslušenství!",
-      description: "Využijte naší aktuální nabídky slev na cyklistické příslušenství. Najdete u nás helmy, zámky, blatníky, pumpy a další potřebné vybavení.",
-      image: "/images/aktuality/medvidek.jpg"
-    },
-    {
-      title: "Bezpečnost na prvním místě!",
-      description: "Jezdíte rádi za tmy? Nezapomeňte na bezpečnostní vybavení! Nabízíme široký výběr cyklistických přileb, reflexních vest a světel. Vaše bezpečnost je naší prioritou!",
-      image: "/images/aktuality/helma.jpg"
-    },
-  ];  
+  {
+    title: "Povánoční servisní prohlídka",
+    description: "Dostali jste pod stromeček nové kolo? Nechte si ho u nás zkontrolovat a seřídit, aby vám dělalo radost co nejdéle. Nabízíme servisní prohlídky za zvýhodněné ceny.",
+    image: "/images/aktuality/servis.jpg"
+  },
+  {
+    title: "Nezapomeňte na reflexní prvky",
+    description: "V zimě je viditelnost na silnici klíčová. Nabízíme široký výběr reflexních prvků pro cyklisty - vesty, pásky, světla a další.",
+    image: "/images/aktuality/podzim.jpg" 
+  },
+  {
+    title: "S dětmi na kole i v zimě? Žádný problém!",
+    description: "Myslíte si, že s příchodem zimy a sněhu končí cyklistická sezóna? S naším vybavením si jízdu na kole s dětmi užijete i v chladnějších měsících. Máme pro vás široký výběr dětských sedaček a vozíků za kola.",
+    image: "/images/aktuality/detska-sedacka.jpg" 
+  },
+  {
+    title: "Naplánujte si letní cyklodovolenou s elektrokolem!",
+    description: "Sníte o letní cyklistické dovolené bez námahy? Pořiďte si elektrokolo a objevujte krásy okolí s lehkostí! Máme pro vás široký výběr elektrokol i cyklistických brašen, nosičů a dalšího vybavení pro cestování na kole.",
+    image: "/images/aktuality/elektro-kolo.jpg" 
+  },
+  {
+    title: "Zimní cyklistika? Žádný problém!",
+    description: "I v zimě se dá na kole jezdit! Máme pro vás široký výběr zimního vybavení pro cyklistiku v chladném počasí.",
+    image: "/images/aktuality/medvidek.jpg" 
+  },
+  {
+    title: "Bezpečnost nadevše i v zimě!",
+    description: "Nezapomeňte, že i v zimě je důležité chránit si hlavu. Přijďte si k nám vybrat novou cyklistickou přilbu a my vám ji zdarma seřídíme.",
+    image: "/images/aktuality/helma.jpg"
+  },
+];
 
 export { aktuality };
